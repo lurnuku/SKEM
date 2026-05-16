c0195e640ca055d260a1fc5b7751de6fc4f1d03b
lurnuku <lurnuku@yandex.com>
2026-05-16 16:13:18 +0200

    feat: add Insert component, configure prettier classnames plugin, and remove bits-ui

---
+++ frontend/src/components/Insert.svelte
@@ -0,0 +1,11 @@
+<form>
+	<div>
+		<input
+			type="text"
+			placeholder="Enter a url"
+			class="focus:none w-80 cursor-pointer rounded-2xl border-none bg-primary p-4 text-base
+				text-dark shadow-[0_0.4rem_#5d1d1e] focus:ring-0 focus-visible:ring-0
+				focus-visible:outline-none active:ring-0 active:outline-none"
+		/>
+	</div>
+</form>
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -3,15 +3,8 @@ pages by adding files to the src/routes directory of your project. These will be
 that a user's first visit to your app is as fast as possible, then a client-side app takes over."
 But I am not sure how to set it up properly yet, gonna have to figure it out */ -->
 
-<!-- Testing bits-ui -->
 <script lang="ts">
-	import { Button } from 'bits-ui'
+	import Insert from '../components/Insert.svelte'
 </script>
 
-<Button.Root
-	class="rounded-input bg-dark text-background shadow-mini hover:bg-dark/95 inline-flex
-	h-12 items-center justify-center px-[21px] text-[15px]
-	font-semibold active:scale-[0.98] active:transition-all"
->
-	Unlimited
-</Button.Root>
+<Insert />
